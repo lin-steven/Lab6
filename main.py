ef94aff02332da715829b5c6f1d09100d70af820
Daniel <daniel.pool.2024@gmail.com>
2024-10-22 02:06:45 +0900

    Added the Decode Function

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # Lab 6 Project
 
-def encode():
-	pass
+def encode(code): #By: Daniel Pool
+	decoded = ""
+	for i in code:
+		decoded += str((int(i) - 3) % 10)
+	return decoded
 
 print("This is Lab 6.")
